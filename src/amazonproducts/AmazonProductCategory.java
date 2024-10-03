cfe5d815cc0305e77387b5d8ceed5b2839160d0c
Courtney Hammond <hamm0235@algonquinlive.com>
2024-10-03 10:41:21 -0400

    Create definitions for category constructor, setter, and getter

--- src/amazonproducts/AmazonProductCategory.java
+++ src/amazonproducts/AmazonProductCategory.java
@@ -2,4 +2,16 @@ package amazonproducts;
 
 public class AmazonProductCategory {
 	private String categoryName;
+	
+	AmazonProductCategory(String givenName){
+		
+	}
+	
+	public void setCategory(String setName){
+		
+	}
+	
+	public String getCategory() {
+		return "category";
+	}
 }
