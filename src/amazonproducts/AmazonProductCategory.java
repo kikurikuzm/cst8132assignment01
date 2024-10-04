1c901730fe4486ffb223d4e46ba71efcb5974ee6
Matthias Kuzma <tophatgaming1205@gmail.com>
2024-10-04 07:55:00 -0400

    restored master contents to match pre-rollback branch

--- src/amazonproducts/AmazonProductCategory.java
+++ src/amazonproducts/AmazonProductCategory.java
@@ -2,4 +2,16 @@ package amazonproducts;
 
 public class AmazonProductCategory {
 	private String categoryName;
+	
+	AmazonProductCategory(String givenName){
+		this.categoryName = givenName;
+	}
+	
+	public void setCategory(String setName){
+		this.categoryName = setName;
+	}
+	
+	public String getCategory() {
+		return this.categoryName;
+	}
 }
--- src/amazonproducts/AmazonProductUtil.java
+++ src/amazonproducts/AmazonProductUtil.java
@@ -1,12 +1,56 @@
 package amazonproducts;
 
+import java.io.*;
+
 public class AmazonProductUtil {
 	public float convertStringToFloat(String desiredString){
-		//return float
+		return Float.parseFloat(desiredString);
 	}
-	
-	public String[] lineReader(String line, int index) {//change variable names when purpose determined
 		
+	public static String[] lineReader(String fileName, int lineIndex) {
+		try {
+			FileReader fileReader = new FileReader(fileName);
+				
+			boolean readingLine = true;
+			int lineCharacterIndex = 0;
+			int currentLineIndex = 0;
+			int currentColumn = 0;
+			String currentString = "";
+			String[] currentCSVArray = new String[10];
+				
+			while (currentLineIndex <= lineIndex) {
+				try {
+					Character currentCharacter = (char)fileReader.read();
+					if(currentCharacter.equals('\n')) {
+						currentLineIndex ++;
+						currentColumn = 0;
+					}
+					if(currentLineIndex == lineIndex && currentCharacter.equals(',')) { //Only doing column stuff on the right line
+						currentCSVArray[currentColumn] = currentString;
+						currentColumn ++;
+						currentString = "";
+					} else {
+						currentString += currentCharacter;
+					}
+					System.out.print(currentCharacter);
+					lineCharacterIndex ++;
+				}
+				catch (IOException e){
+					System.out.println("IOException");
+					System.out.println(e);
+				}
+				
+			}
+			
+			return currentCSVArray; //Return the collection of columns for the corresponding line
+		}
+		catch (FileNotFoundException e){
+			System.out.println("File not found.");
+			System.out.println(e);
+		}
+		
+		return new String[1]; //Returning small array when function fails
+			  
 	}
 	
 }
